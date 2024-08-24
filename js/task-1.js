058fba271f26f5bdf51343e6893dab74da14f174
Anton Bolestiev <antonbolestiev1997@gmail.com>
2024-08-24 16:17:33 +0300

    add changes in 1 and 6 tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,33 +1,43 @@
-const liItem = document.querySelectorAll(".item");
-const cont = document.querySelector("#categories");
-const upperLi = document.querySelectorAll("#categories li ul");
-const lowestLi = document.querySelectorAll("#categories .item ul li");
-lowestLi.forEach((element) => {
+const liItems = document.querySelectorAll(".item");
+
+const categoriesContainer = document.querySelector("#categories");
+
+const nestedLists = document.querySelectorAll("#categories > li > ul");
+const allItems = document.querySelectorAll("#categories .item ul li");
+
+allItems.forEach((element) => {
   element.style.border = "1px solid #808080";
   element.style.borderRadius = "4px";
   element.style.padding = "8px 16px";
 });
-upperLi.forEach((element) => {
-  element.style.listStyleType = "none";
-  element.style.padding = "0";
-  element.style.display = "flex";
-  element.style.flexDirection = "column";
-  element.style.gap = "8px";
+
+nestedLists.forEach((ul) => {
+  ul.style.listStyleType = "none";
+  ul.style.padding = "0";
+  ul.style.display = "flex";
+  ul.style.flexDirection = "column";
+  ul.style.gap = "8px";
 });
-cont.style.display = "flex";
-cont.style.flexDirection = "column";
-cont.style.gap = "16px";
-console.log(`Number of categoties: ${liItem.length}`);
 
-liItem.forEach((element) => {
-  element.style.backgroundColor = "#F6F6FE";
-  element.style.listStyleType = "none";
-  element.style.padding = "16px";
-  element.style.display = "flex";
-  element.style.flexDirection = "column";
-  element.style.gap = "16px";
-  element.children[0].style.margin = "0";
+categoriesContainer.style.display = "flex";
+categoriesContainer.style.flexDirection = "column";
+categoriesContainer.style.gap = "16px";
 
-  console.log(`Category: ${element.children[0].textContent}`);
-  console.log(`Elements: ${element.lastElementChild.children.length}`);
+console.log(`Number of categories: ${liItems.length}`);
+
+liItems.forEach((item) => {
+  item.style.backgroundColor = "#F6F6FE";
+  item.style.listStyleType = "none";
+  item.style.padding = "16px";
+  item.style.display = "flex";
+  item.style.flexDirection = "column";
+  item.style.gap = "16px";
+  item.children[0].style.margin = "0";
+
+  const header = item.querySelector("h2").textContent;
+
+  const itemCount = item.querySelectorAll("ul > li").length;
+
+  console.log(`Category: ${header}`);
+  console.log(`Elements: ${itemCount}`);
 });
